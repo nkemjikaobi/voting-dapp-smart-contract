1cf53b6c76b3ff4ab95151847848e72eeaa60a24
Obi Nkemjika Derick <obinkemjikaderick@Administrators-MacBook-Pro.local>
2022-04-21 23:11:44 +0100

    cleanup

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -6,21 +6,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
-
+  
   const Voting = await hre.ethers.getContractFactory("Voting");
   const voting = await Voting.deploy();
 
@@ -29,8 +15,6 @@ async function main() {
   console.log("Voting Contract:", voting.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
